88432aa67ce4d4e6b83dd8ea64d1fed31422c668
abr-ya <gamer05@list.ru>
2021-03-02 13:47:53 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  return Number(String(n).replace('-','').split('').reverse().join(''));
 }
+
+const test = (n) => Number(String(n).split('').reverse().join(''));
+
+console.log(test(123));
